cab8b6833ef340fe1d19f686cf13de3160b907cd
Julio <paespintoj@gmail.com>
2024-09-10 08:40:13 -0300

    Alterações de Show/Hide password

--- cadastro.php
+++ cadastro.php
@@ -15,40 +15,34 @@
                         <div class="input-group">
                             <input type="email" name="email" required class="form-control" style="color: #13292A; background-color:#78CEBA;" placeholder="Email">
                         </div>
-                        <div class="form-group">
+                    </div>
+                    <div class="form-group">
                         <label for="senha" style="color: #13292A;">Senha</label>
                         <div class="input-group">
                             <input type="password" name="senha1" required class="form-control" style="color: #13292A; background-color:#78CEBA;" id="senha" placeholder="Password">
                             <div class="input-group-append">
-                                <button type="button" class="btn" id="togglePassword" style="background-color:#78CEBA; color: #13292A; border: none;">
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="17" height="17" fill="currentColor" class="bi bi-eye" viewBox="0 0 16 16">
-                                        <path d="M16 8s-3-5.5-8-5.5S0 8 0 8s3 5.5 8 5.5S16 8 16 8M1.173 8a13 13 0 0 1 1.66-2.043C4.12 4.668 5.88 3.5 8 3.5s3.879 1.168 5.168 2.457A13 13 0 0 1 14.828 8q-.086.13-.195.288c-.335.48-.83 1.12-1.465 1.755C11.879 11.332 10.119 12.5 8 12.5s-3.879-1.168-5.168-2.457A13 13 0 0 1 1.172 8z"/>
-                                        <path d="M8 5.5a2.5 2.5 0 1 0 0 5 2.5 2.5 0 0 0 0-5M4.5 8a3.5 3.5 0 1 1 7 0 3.5 3.5 0 0 1-7 0"/>
-                                    </svg>
+                                <button type="button" class="btn" id="togglePassword" style="background-color:#78CEBA; color: #13292A; border: none; margin-left: 5px;" onclick="togglePasswordVisibility1()">
+                                    <img src="Imgs/Eye.png" height="17" width="17" alt="" id="toggleImage1">
                                 </button>
                             </div>
                         </div>
                     </div>
                     <div class="form-group">
-                    <label for="senha" style="color: #13292A;">Senha</label>
+                        <label for="senha2" style="color: #13292A;">Confirmar Senha</label>
                         <div class="input-group">
                             <input type="password" name="senha2" required class="form-control" style="color: #13292A; background-color:#78CEBA;" id="senha2" placeholder="Password">
                             <div class="input-group-append">
-                                <button type="button" class="btn" id="togglePassword2" style="background-color:#78CEBA; color: #13292A; border: none;">
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="17" height="17" fill="currentColor" class="bi bi-eye" viewBox="0 0 16 16">
-                                        <path d="M16 8s-3-5.5-8-5.5S0 8 0 8s3 5.5 8 5.5S16 8 16 8M1.173 8a13 13 0 0 1 1.66-2.043C4.12 4.668 5.88 3.5 8 3.5s3.879 1.168 5.168 2.457A13 13 0 0 1 14.828 8q-.086.13-.195.288c-.335.48-.83 1.12-1.465 1.755C11.879 11.332 10.119 12.5 8 12.5s-3.879-1.168-5.168-2.457A13 13 0 0 1 1.172 8z"/>
-                                        <path d="M8 5.5a2.5 2.5 0 1 0 0 5 2.5 2.5 0 0 0 0-5M4.5 8a3.5 3.5 0 1 1 7 0 3.5 3.5 0 0 1-7 0"/>
-                                    </svg>
+                                <button type="button" class="btn" id="togglePassword2" style="background-color:#78CEBA; color: #13292A; border: none; margin-left: 5px;" onclick="togglePasswordVisibility2()">
+                                    <img src="Imgs/Eye.png" height="17" width="17" alt="" id="toggleImage2">
                                 </button>
                             </div>
                         </div>
-                    </div>
                     </div>
                     <div class="d-flex justify-content-center">
                         <button type="submit" style="background-color:#78CEBA; color: #13292A;" class="btn mt-2 d-flex justify-content-center w-75">Cadastrar</button>
                     </div>
                     <p class="text-center mt-1">
-                        Já tem uma conta? <a href="index.php?p=6" style="color: #78CEBA;" >Faça seu login!</a>
+                        Já tem uma conta? <a href="index.php?p=5" style="color: #78CEBA;">Faça seu login!</a>
                     </p>
                 </form>
             </div>
@@ -56,29 +50,30 @@
     </div>
 </div>
 
-
 <script>
-    document.getElementById('togglePassword').addEventListener('click', function() {
-        const senhaInput = document.getElementById('senha');
-        const icon = document.getElementById('togglePassword').querySelector('svg');
-        const type = senhaInput.getAttribute('type') === 'password' ? 'text' : 'password';
-        senhaInput.setAttribute('type', type);
-        if (type === 'password') {
-            icon.setAttribute('class', 'bi bi-eye');
-        } else {
-            icon.setAttribute('class', 'bi bi-eye-slash');
-        }
-    });
+    function togglePasswordVisibility1() {
+        var passwordField = document.getElementById('senha');
+        var toggleImage = document.getElementById('toggleImage1');
 
-    document.getElementById('togglePassword2').addEventListener('click', function() {
-        const senhaInput = document.getElementById('senha2');
-        const icon = document.getElementById('togglePassword2').querySelector('svg');
-        const type = senhaInput.getAttribute('type') === 'password' ? 'text' : 'password';
-        senhaInput.setAttribute('type', type);
-        if (type === 'password') {
-            icon.setAttribute('class', 'bi bi-eye');
+        if (passwordField.type === "password") {
+            passwordField.type = "text";
+            toggleImage.src = "Imgs/Hide.png";
         } else {
-            icon.setAttribute('class', 'bi bi-eye-slash');
+            passwordField.type = "password";
+            toggleImage.src = "Imgs/Eye.png";
         }
-    });
+    }
+
+    function togglePasswordVisibility2() {
+        var passwordField = document.getElementById('senha2');
+        var toggleImage = document.getElementById('toggleImage2');
+
+        if (passwordField.type === "password") {
+            passwordField.type = "text";
+            toggleImage.src = "Imgs/Hide.png";
+        } else {
+            passwordField.type = "password";
+            toggleImage.src = "Imgs/Eye.png";
+        }
+    }
 </script>
--- login.php
+++ login.php
@@ -18,11 +18,8 @@
                         <div class="input-group">
                             <input type="password" name="senha" class="form-control" style="color: #13292A; background-color:#78CEBA;" id="senha" placeholder="Password">
                             <div class="input-group-append">
-                                <button type="button" class="btn" id="togglePassword" style="background-color:#78CEBA; color: #13292A; border: none;">
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="17" height="17" fill="currentColor" class="bi bi-eye" viewBox="0 0 16 16">
-                                        <path d="M16 8s-3-5.5-8-5.5S0 8 0 8s3 5.5 8 5.5S16 8 16 8M1.173 8a13 13 0 0 1 1.66-2.043C4.12 4.668 5.88 3.5 8 3.5s3.879 1.168 5.168 2.457A13 13 0 0 1 14.828 8q-.086.13-.195.288c-.335.48-.83 1.12-1.465 1.755C11.879 11.332 10.119 12.5 8 12.5s-3.879-1.168-5.168-2.457A13 13 0 0 1 1.172 8z"/>
-                                        <path d="M8 5.5a2.5 2.5 0 1 0 0 5 2.5 2.5 0 0 0 0-5M4.5 8a3.5 3.5 0 1 1 7 0 3.5 3.5 0 0 1-7 0"/>
-                                    </svg>
+                                <button type="button" class="btn" id="togglePassword" style="background-color:#78CEBA; color: #13292A; border: none; margin-left: 5px;" onclick="togglePasswordVisibility()">
+                                    <img src="Imgs/Eye.png" height="17" width="17" alt="" id="toggleImage">
                                 </button>
                             </div>
                         </div>
@@ -45,16 +42,16 @@
 </div>
 
 <script>
-    document.getElementById('togglePassword').addEventListener('click', function() {
-        const senhaInput = document.getElementById('senha');
-        const icon = document.getElementById('togglePassword').querySelector('svg');
-        const type = senhaInput.getAttribute('type') === 'password' ? 'text' : 'password';
-        senhaInput.setAttribute('type', type);
-        if (type === 'password');
-    });
+    function togglePasswordVisibility() {
+        var passwordField = document.getElementById('senha');
+        var toggleImage = document.getElementById('toggleImage');
 
-    history.pushState(null, null, document.URL);
-window.addEventListener('popstate', function () {
-    history.pushState(null, null, document.URL);
-});
+        if (passwordField.type === "password") {
+            passwordField.type = "text";
+            toggleImage.src = "Imgs/Hide.png";
+        } else {
+            passwordField.type = "password";
+            toggleImage.src = "Imgs/Eye.png";
+        }
+    }
 </script>
