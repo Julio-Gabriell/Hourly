86c783fd855994bfcccc570857e1d5166ba31b06
Julio <paespintoj@gmail.com>
2024-10-08 12:59:16 -0300

    Hourly

--- Login/topo.php
+++ Login/topo.php
@@ -21,7 +21,7 @@ verificarLogin();
         <header class="d-flex flex-wrap align-items-center justify-content-md-between py-3 mb-4 border-bottom">
             <div class="col-md-3 mb-2 mb-md-0 justify-content-center">
                 <a href="home_logado.php">
-                    <img src="../Imgs/logo.png" alt=""></img>
+                    <img src="../Imgs/logo.png" alt="Logo">
                 </a>
             </div>
 
@@ -56,6 +56,10 @@ verificarLogin();
                 $conn->close();
                 ?>
             </a>
+
         </header>
     </div>
     <div class="container">
+</body>
+
+</html>
