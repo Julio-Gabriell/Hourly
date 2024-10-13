3b48f8c4b6163b6b38655cb3f3717b20924c72b5
Julio <paespintoj@gmail.com>
2024-10-12 23:06:57 -0300

    home dono ajusatada

--- Dono/home_dono.php
+++ Dono/home_dono.php
@@ -1,26 +1,25 @@
 <?php
 
 include_once "topo.php"; ?>
-
-<div class="row p-4 pb-0 pe-lg-0 pt-lg-5 align-items-center ">
-  <div class="col-lg-7 p-3 p-lg-5 pt-lg-3">
-    <h1 class="display-4 fw-bold lh-1 text-body-emphasis">Agende seu estilo, agilize seu dia: Hourly,
-      onde cada horário é um passo para sua melhor aparência!</h1>
-    <p class="lead pt-3">Hourly revoluciona o agendamento para barbearias,
-      unindo clientes e profissionais em uma experiência inovadora.
-      Com foco na excelência e facilidade de uso,
-      conectamos comunidades locais e estabelecimentos de beleza.</p>
+<div class="container">
+  <div class="row p-4 pb-0 pe-lg-0 pt-lg-5 align-items-center ">
+    <div class="col-lg-7 p-3 p-lg-5 pt-lg-3">
+      <h1 class="display-4 fw-bold lh-1 text-body-emphasis">Agende seu estilo, agilize seu dia: Hourly,
+        onde cada horário é um passo para sua melhor aparência!</h1>
+      <p class="lead pt-3">Hourly revoluciona o agendamento para barbearias,
+        unindo clientes e profissionais em uma experiência inovadora.
+        Com foco na excelência e facilidade de uso,
+        conectamos comunidades locais e estabelecimentos de beleza.</p>
+    </div>
+    <div class="col-lg-4 offset-lg-1 p-0 overflow-hidden">
+      <img class="rounded-lg-3" src="../Imgs/postezinho.png" alt="" width="500">
+    </div>
   </div>
-  <div class="col-lg-4 offset-lg-1 p-0 overflow-hidden">
-    <img class="rounded-lg-3" src="../Imgs/postezinho.png" alt="" width="500">
-  </div>
-</div>
-</div>
 <h1 style="color: #13292A;" class="text-center">
   Nosso Time
 </h1>
 <div class="container px-4 py-5">
-  <div class="row g-4 py-5 row-cols-1 row-cols-lg-3">
+  <div class="row g-3 row-cols-1 row-cols-md-3">
     <div class="col d-flex align-items-start">
       <div
         class="icon-square text-body-emphasis d-inline-flex align-items-center justify-content-center fs-4 flex-shrink-0 me-3">
@@ -70,6 +69,7 @@ include_once "topo.php"; ?>
       </div>
     </div>
   </div>
+</div>
 </div>
 <?php
 
